998a2f76147e61e50785cc7d17f5d4410c9700d3
reklast <reklast51@gmail.com>
2024-02-02 14:37:41 +0200

    accidental commit of unnecessary files fix

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 
 import Map from '@components/Map'
-import CarouselComponent from '@components/CarouselComponent/CarouselComponent'
 
 const MapPage = () => (
   <div>
