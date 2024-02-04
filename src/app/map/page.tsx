fef3d62fbd630dc06456bc04353c65d4bc4942cc
reklast <reklast51@gmail.com>
2024-02-04 19:44:41 +0200

    dynamic import added for carousel component

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 
 import Map from '@components/Map'
+
+const DynamicCarousel = dynamic(() => import('@components/CarouselComponent/CarouselComponent'), {
+  ssr: false,
+})
 
 const MapPage = () => (
   <div>
@@ -18,6 +23,7 @@ const MapPage = () => (
       />
     </Head>
     <Map />
+    <DynamicCarousel />
   </div>
 )
 
