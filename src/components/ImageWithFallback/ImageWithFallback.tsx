76c963ca2e871b740db750ce5e6fe24d5a74283c
reklast <reklast51@gmail.com>
2024-02-07 19:24:31 +0200

    whitespace fix(prettier)

--- src/components/ImageWithFallback/ImageWithFallback.tsx
+++ src/components/ImageWithFallback/ImageWithFallback.tsx
@@ -1,19 +1,20 @@
 'use client'
-import { useState } from 'react';
+
+import { useState } from 'react'
 
 const ImageWithFallback = (props: any) => {
-    const { src, fallback } = props;
-    const [imgSrc, setImgSrc] = useState(src);
+  const { src, fallback } = props
+  const [imgSrc, setImgSrc] = useState(src)
 
-    return (
-        <img
-            {...props}
-            src={imgSrc}
-            onError={() => {
-                setImgSrc(fallback);
-            }}
-        />
-    );
-};
+  return (
+    <img
+      {...props}
+      src={imgSrc}
+      onError={() => {
+        setImgSrc(fallback)
+      }}
+    />
+  )
+}
 
-export default ImageWithFallback;
+export default ImageWithFallback
