7ee0f3362a4acf17937456783faebc69a9ca95f9
reklast <reklast51@gmail.com>
2024-02-08 14:20:42 +0200

    tailwind config fix

--- src/components/lib/helper/getTailwindConfig.ts
+++ src/components/lib/helper/getTailwindConfig.ts
@@ -1,3 +1,6 @@
+import { RecursiveKeyValuePair, ResolvableTo } from 'tailwindcss/types/config'
+
 import twConfig from '@root/tailwind.config.js'
 
-export const twColor = (color: string) => twConfig.theme.colors[color]
+export const twColor = (color: string) =>
+  twConfig?.theme?.colors![color as keyof ResolvableTo<RecursiveKeyValuePair<string, string>>]
