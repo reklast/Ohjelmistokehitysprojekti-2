27a1fc5658f612011b098e91951e3fdbff5add29
Nikita <reklast51@gmail.com>
2024-02-15 10:37:18 +0200

    removed center card function(maybe we should keep it idk)

--- src/components/Map/MapContextProvider.tsx
+++ src/components/Map/MapContextProvider.tsx
@@ -4,7 +4,6 @@ import { createContext, useCallback, useState } from 'react'
 interface MapContextValues {
   map: Leaflet.Map | undefined;
   setMap: (e: Leaflet.Map | undefined) => void;
-  centerCard: (lat: number, lng: number, zoom: number) => void;
 }
 
 export const MapContext = createContext<MapContextValues | undefined>(undefined)
@@ -16,14 +15,7 @@ interface MapContextProviderProps {
 const MapContextProvider = ({ children }: MapContextProviderProps) => {
   const [map, setMap] = useState<Leaflet.Map | undefined>(undefined)
 
-  const centerCard = useCallback((lat: number, lng: number, zoom: number) => {
-    console.log("Centering map to:", lat, lng, "with zoom:", zoom);
-    if (map) {
-      map.flyTo([lat, lng], zoom);
-    }
-  }, [map]);
-
-  return <MapContext.Provider value={{ map, setMap, centerCard }}>{children}</MapContext.Provider>
+  return <MapContext.Provider value={{ map, setMap }}>{children}</MapContext.Provider>
 }
 
 export default MapContextProvider
