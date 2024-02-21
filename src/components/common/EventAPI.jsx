d6802420387067153a2a953e68c3c4dd5e25c01a
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2024-02-21 16:58:29 +0200

    More Styling

--- src/components/common/EventAPI.jsx
+++ src/components/common/EventAPI.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { black } from 'tailwindcss/colors';
+import React, { useEffect, useState } from 'react'
+import { black } from 'tailwindcss/colors'
 
-const EventsAPI = ({display}) => {
-  const [data, setData] = useState(null);
-  
+const EventsAPI = ({ display }) => {
+  const [data, setData] = useState(null)
+
   const fetchData = async () => {
-    const endpoint = 'https://api.hel.fi/linkedevents/v1/event/?local_ongoing_OR=musiikki&format=json';
+    const endpoint = 'https://api.hel.fi/linkedevents/v1/event/?musiikki_OR=konsertti&format=json'
 
     try {
-      const response = await fetch(endpoint);
+      const response = await fetch(endpoint)
 
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`)
       }
 
-      const responseData = await response.json();
-      setData(responseData.data); // Assuming the actual data is nested under the 'data' property
+      const responseData = await response.json()
+      setData(responseData.data) // Assuming the actual data is nested under the 'data' property
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const styles = {
     container: {
@@ -31,43 +31,39 @@ const EventsAPI = ({display}) => {
       border: '1px solid black',
       paddingTop: '25px',
       padding: '10px',
-      height: '100vh', 
-      width: '75vw', 
+      height: '60vh',
+      width: '75vw',
       overflowY: 'auto',
-      
     },
-    
-  };
+  }
   const h1Styles = {
-    container:{
+    container: {
       padding: '50px',
       fontSize: '26px',
-      color: 'black'
-    }
+      color: 'black',
+    },
   }
-if (display)
-{  return (
-    <div style={styles.container}>
-      <h1 style={{backgroundColor: 'green',color: black, padding: 30}} >Tapahtumat</h1>
-      <div style={{ maxHeight: '100%', overflowY: 'auto' }}>
-        {data && Array.isArray(data) && data.length > 0 ? (
-          data.map((item, index) => (
-            <div key={index}>
-            <h1 style={h1Styles.container} dangerouslySetInnerHTML={{ __html: item.name.fi }} />
-              <div dangerouslySetInnerHTML={{ __html: item.description.fi }} />
-            </div>
-          ))
-        ) : (
-          <p>Ladataan</p>
-        )}
+  if (display) {
+    return (
+      <div style={styles.container}>
+        <h1 style={{ color: black, padding: 30 }}>Tapahtumat</h1>
+        <div style={{ maxHeight: '100%', overflowY: 'auto' }}>
+          {data && Array.isArray(data) && data.length > 0 ? (
+            data.map((item, index) => (
+              <div key={index}>
+                <h1 style={h1Styles.container} dangerouslySetInnerHTML={{ __html: item.name.fi }} />
+                <div dangerouslySetInnerHTML={{ __html: item.description.fi }} />
+              </div>
+            ))
+          ) : (
+            <p>Ladataan</p>
+          )}
+        </div>
       </div>
-    </div>
-  );
-} else {
-  return null; 
+    )
+  } else {
+    return null
+  }
 }
 
-  
-};
-
-export default EventsAPI;
+export default EventsAPI
