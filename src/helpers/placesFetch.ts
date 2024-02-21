96392c15941cb20cb4b384c750450f7123748aef
reklast <reklast51@gmail.com>
2024-02-21 11:45:59 +0200

    placesFetch function updated

--- src/helpers/placesFetch.ts
+++ src/helpers/placesFetch.ts
@@ -1,5 +1,5 @@
-const placesFetch = async () => {
-  const data = await fetch('https://www.hel.fi/palvelukarttaws/rest/v4/unit/?search=museo')
+const placesFetch = async (query?: string) => {
+  const data = await fetch(`https://www.hel.fi/palvelukarttaws/rest/v4/unit/?search=${query ? query : 'muistomerkki'}`)
 
   return await data.json()
 }
