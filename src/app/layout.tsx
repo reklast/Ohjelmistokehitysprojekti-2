aca8a3557510a7c2917e890d0e3928bab3e2931a
reklast <reklast51@gmail.com>
2024-02-23 18:21:39 +0200

    provider component and layout content wrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import 'leaflet/dist/leaflet.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { Providers } from '@components/Providers/Providers'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
