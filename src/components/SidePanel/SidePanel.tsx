d8eea5a32bfff6317bff9753b023de619ed447aa
reklast <reklast51@gmail.com>
2024-03-12 19:05:10 +0200

    responsiveness for mobile

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,13 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import useMapContext from '@components/Map/useMapContext'
 
 function SidePanel() {
   const { currentTarget, sidePanel, setSidePanel } = useMapContext()
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
   const panelContainer = useRef<HTMLDivElement>(null)
-  sidePanel
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [window])
+
+  screenWidth > 660 && sidePanel
     ? panelContainer.current?.style.setProperty('width', '40%')
-    : panelContainer.current?.style.setProperty('width', '0')
+    : screenWidth < 640 && sidePanel
+    ? panelContainer.current?.style.setProperty('width', '100%')
+    : panelContainer.current?.style.setProperty('width', '0%')
 
   const closePanel = () => {
     panelContainer.current?.style.setProperty('width', '0')
@@ -16,9 +26,9 @@ function SidePanel() {
   return (
     <div
       ref={panelContainer}
-      className="transition-all duration-500 z-[2000] fixed overflow-x-hidden h-[calc(100%-5rem)] bg-dark opacity-95 w-0 right-0 bottom-0"
+      className="transition-all duration-500 z-[2000] fixed h-[calc(100%-5rem)] overflow-scroll bg-dark opacity-95 w-0 right-0 bottom-0"
     >
-      <div className="flex flex-col justify-between overflow-scroll items-center text-white m-8">
+      <div className="flex flex-col justify-between items-center text-white m-8">
         <div className="flex w-full items-end">
           <button onClick={() => closePanel()} className="scale-[2] ml-4 mb-4 h-5">
             &times;
